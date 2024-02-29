b6db8622caaf43267482a1578d0fcd24798ec09b
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2024-02-29 15:13:26 +0100

    env

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://localhost:7055/api', // Replace with your API base URL
+  //baseURL: 'https://localhost:7055/api', // dev
+  baseURL: 'https://authentguardapi-prod.azurewebsites.net/api', // prod
   timeout: 5000, // Set a reasonable timeout
 });
 
